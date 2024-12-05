cb752d1f2811578064a26e5552e0d9a18ec314e8
Jinx-means-jinx <www.2927196163@qq.com>
2024-12-05 12:33:48 +1100

    rebase

--- public/usage-examples/graphics/clear_screen/clear_screen-1-background-color.cs
+++ public/usage-examples/graphics/clear_screen/clear_screen-1-background-color.cs
@@ -1,6 +1,10 @@
 using static SplashKitSDK.SplashKit;
 
+<<<<<<< HEAD:public/usage-examples/graphics/clear_screen/clear_screen-1-background-color.cs
 OpenWindow("Blue Background", 800, 600);
+=======
+OpenWindow("Back Ground", 800, 600);
+>>>>>>> cac3e39 (change the house drawing to background in C# code):src/assets/usage-examples-code/graphics/clear_screen/clear_screen-1-background-color.cs
 
 ClearScreen(ColorBlue());
 RefreshScreen(60);
