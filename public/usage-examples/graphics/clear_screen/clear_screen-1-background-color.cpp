5f8900297148c253395b7db62b735582ed413426
Jinx-means-jinx <www.2927196163@qq.com>
2024-12-05 12:34:01 +1100

    rebase

--- public/usage-examples/graphics/clear_screen/clear_screen-1-background-color.cpp
+++ public/usage-examples/graphics/clear_screen/clear_screen-1-background-color.cpp
@@ -5,6 +5,12 @@
 int main()
 {
     open_window("Blue Background", 800, 600);
+<<<<<<< HEAD:public/usage-examples/graphics/clear_screen/clear_screen-1-background-color.cpp
+=======
+    clear_screen();
+    refresh_screen(60);
+    delay(1000);
+>>>>>>> ff3ab6b (change the title to blue background):src/assets/usage-examples-code/graphics/clear_screen/clear_screen-1-background-color.cpp
     
     clear_screen(COLOR_BLUE);
     refresh_screen(60);
--- public/usage-examples/graphics/clear_screen/clear_screen-1-simple.cs
+++ public/usage-examples/graphics/clear_screen/clear_screen-1-simple.cs
@@ -1,6 +1,13 @@
 using static SplashKitSDK.SplashKit;
 
 OpenWindow("Blue Background", 800, 600);
+<<<<<<< HEAD:public/usage-examples/graphics/clear_screen/clear_screen-1-simple.cs
+=======
+
+ClearScreen();
+RefreshScreen(60);
+Delay(1000);
+>>>>>>> ff3ab6b (change the title to blue background):src/assets/usage-examples-code/graphics/clear_screen/clear_screen-1-background-color.cs
 
 ClearScreen(ColorBlue());
 RefreshScreen(60);
