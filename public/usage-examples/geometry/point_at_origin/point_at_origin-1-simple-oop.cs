5d40fe32ca9c3010144adaea2f9a7e155a23444a
Jinx-means-jinx <www.2927196163@qq.com>
2025-01-12 17:03:20 +1100

    .

--- public/usage-examples/geometry/point_at_origin/point_at_origin-1-simple-oop.cs
+++ public/usage-examples/geometry/point_at_origin/point_at_origin-1-simple-oop.cs
@@ -1,23 +1,23 @@
 using SplashKitSDK;
 
-namespace PointAt
+namespace PointAtExample
 {
     public class Program
     {
         public static void Main()
         {
-        SplashKit.OpenWindow("Point At Origin", 800, 600);
-        SplashKit.ClearScreen();
+            Window window = new Window("Point At Origin", 800, 600);
+            window.Clear(Color.White);
 
-        // Create a point at origin
-        Point2D Point = SplashKit.PointAtOrigin();
+            // Create a point at origin
+            Point2D Point = SplashKit.PointAtOrigin();
 
-        // Create circle at the origin point
-        SplashKit.FillCircle(Color.Red, Point.X, Point.Y, 4);
+            // Create circle at the origin point
+            SplashKit.FillCircle(Color.Red, Point.X, Point.Y, 4);
 
-        SplashKit.RefreshScreen();
-        SplashKit.Delay(4000);
-        SplashKit.CloseAllWindows();
+            window.Refresh();
+            SplashKit.Delay(4000);
+            window.Close();
         }
     }
 }
